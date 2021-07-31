d39be1b031133e16d3090281bf227602840b823c
Jai Sinha <jaioff02@gmail.com>
2021-08-01 02:39:47 +0530

    added message in Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+ll m;
+cin>>m;
+whille(m--)
+{
+    ll a,b;
+    cin>>a>>b;
+    cout<<"Sum is"<<a+b<<endl;
+}
+return 0;
+}
