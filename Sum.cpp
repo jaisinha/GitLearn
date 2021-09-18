84dcefc2c5b67b8d1f7d82dddef5170d3581c6be
Jai Sinha <jaioff02@gmail.com>
2021-09-18 17:58:43 +0530

    initial commit

--- Sum.cpp
+++ Sum.cpp
@@ -6,9 +6,16 @@ ll m;
 cin>>m;
 whille(m--)
 {
-    ll a,b;
-    cin>>a>>b;
-    cout<<"Sum is"<<a+b<<endl;
+    ll N;
+    cin>>N;
+    ll a[N];
+    for(int i=0;i<N;i++)
+    cin>>a[i];
+    ll M;
+    cin>>m;
+    ll B[M];
+    for(int i=0;i<M;i++)
+    cin>>B[i];
 }
 return 0;
 }
